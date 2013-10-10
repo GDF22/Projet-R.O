7a002040c8b707a74b419ef9d69c0dbbf1bea13e
dubourgfel <dubourgfel@eitsi.eu>
2013-10-10 11:41:35 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 10/10/13
+ * Time: 11:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+}
