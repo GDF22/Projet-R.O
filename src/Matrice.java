8fd38b4a6b5379305383b705b278b647efde85d6
GDF22 <nycgdf@gmail.com>
2013-10-30 17:18:23 +0100

    coût d'éviction

--- src/Matrice.java
+++ src/Matrice.java
@@ -25,11 +25,6 @@ public class Matrice {
 		this.matrice = new Case[taille][taille];
 		for(int i = 0; i < taille; i++) {
 			for(int j = 0; j < taille; j++) {
-				if(i == j) {
-					this.matrice[i][j].setValeur(-1);
-				}
-
-
 				this.matrice[i][j] = new Case(matrice[i][j]);
 				this.matrice[i][j].marque = false;
 			}
@@ -75,6 +70,32 @@ public class Matrice {
 		return(ret);
 	}
 
+    public void reductionLigne() {
+        int min;
+        for(int i = 0; i < this.getTaille(); i++) {
+            min = this.getValue(i, 0);
+            for(int j = 0; j < this.getTaille(); j++) {
+                min = (this.getValue(i, j) < min)? this.getValue(i, j): min;
+            }
+            for(int j = 0; j < this.getTaille(); j++) {
+                this.setValue(i, j, this.getValue(i, j) - min);
+            }
+        }
+    }
+
+    public void reductionColonne() {
+        int min;
+        for(int j = 0; j < this.getTaille(); j++) {
+            min = this.getValue(0, j);
+            for(int i = 0; i < this.getTaille(); i++) {
+                min = (this.getValue(i, j) < min)? this.getValue(i, j): min;
+            }
+            for(int i = 0; i < this.getTaille(); i++) {
+                this.setValue(i, j, this.getValue(i, j) - min);
+            }
+        }
+    }
+
 	public Matrice copy() {
 		Matrice copie = new Matrice(this.taille);
 		for(int i = 0; i < taille; i++) {
@@ -85,6 +106,37 @@ public class Matrice {
 
 		return(copie);
 	}
+
+    public Matrice EvictionCost(){
+        Matrice res = this.copy();
+        int[] minByRow = new int[res.taille];
+        int[] minByColumn = new int[res.taille];
+
+        for (int i = 0 ; i < res.taille; i++){
+            for (int j = 0 ; j < res.taille; j++){
+                minByRow[i] = (this.getValue(i,j)<minByRow[i])? this.getValue(i,j) : minByRow[i];
+                minByColumn[j] = (this.getValue(i,j)<minByColumn[j])? this.getValue(i,j) : minByColumn[j];
+            }
+        }
+        for (int i = 0 ; i < res.taille; i++){
+            for (int j = 0 ; j < res.taille; j++){
+                res.setValue(i,j,minByRow[i]+minByColumn[j]);
+            }
+        }
+       return res;
+    }
+
+    public void suppRowNColumn(int row , int column){
+        this.taille --;
+        Case[][] newmat = new Case[this.taille][this.taille];
+        for(int i = 0 ; i < this.taille+1 ; i++ ){
+            for(int j = 0; j < this.taille+1 ; j++ ){
+                if(i != row && j != column){
+                    newmat[i][j]
+                }
+            }
+        }
+    }
 /*----------------------------------------------*/
 
 
